332cc9e6725cc0b19d39d2de5b1f7bc8070c96d7
Gio <gmpowers774@gmail.com>
2026-03-25 22:17:15 -0400

    Update gulpfile.js

--- gulpfile.js
+++ gulpfile.js
@@ -1,1 +1,27 @@
-var gulp = require('gulp', 'gulp-babel');
+const {src, dest} = require('gulp'),
+    CSSLinter = require('gulp-stylelint'),
+    babel = require(gulp-babel),
+    javaScriptLinting = require('gulp-eslint'),
+    browser = require(browser-sync),
+    reload = browser.reload;
+
+    let userBrowser = 'default';
+
+    async function firefox () {
+        userBrowser = 'firefox';
+    }
+
+    async function brave () {
+        userBrowser = 'brave browser';
+    }
+
+    async function browserList () {
+        userBrowser = [
+            'firefox',
+            'brave browser'
+        ];
+    }
+
+    let lintCSS = () => {
+        
+    }
